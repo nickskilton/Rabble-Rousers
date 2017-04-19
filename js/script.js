dc809891673124b5f00d81b71c48fc10713aca24
nickskilton <nick.skilton@hotmail.com>
2017-04-19 13:30:10 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ $(function(){
 
     // YOU MUST EDIT THIS SECTION
     // You must supply a Trove API key
-    var troveAPIKey = '';
+    var troveAPIKey = '7bqq297enpskqc3b';
     // Either provide full API query here or include options below
     var apiQuery = '';
     // Words you want to search for -- separate multiple values with spaces, eg:
     // var keywords = 'weather wragge';
-    var keywords = '';
+    var keywords = 'anarchist union';
     // How you want to combine keywords -- all, any, or phrase
     var keywordType = 'all'
     // Newspaper id numbers -- separate multiple values with spaces, eg:
     // var titles = '840 35';
     var titles = '';
     // Add a byline, eg:
-    var byline = 'Created by <a href="https://timsherratt.org">Tim Sherratt</a>.'
+    var byline = 'Created by Nick Skilton'
     // var byline = '';
     // Add a tagline
-    var tagline = 'How well do you know your Australian history?';
+    var tagline = 'Are unionists also anarchists?';
     // Leave this alone unless you're publishing on a non-https server
     var useHttps = 'true';
 
